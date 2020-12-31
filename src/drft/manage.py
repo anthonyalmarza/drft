12e0ffe8b70f6dd45729d1395df823ddd9a7cbf7
Anthony Almarza <anthony.almarza@gmail.com>
2020-12-31 11:04:11 -0500

    update manage interface

--- src/drft/manage.py
+++ src/drft/manage.py
@@ -1,11 +1,17 @@
+import os
+from typing import Optional
+
 from django.core.management import execute_from_command_line
 
 
-def manage_factory(manage_py_path: str):
+def manage_factory(manage_py_path: Optional[str] = None):
     """
     Usage:
         manage = manage_factory("my_project/manage.py")
         manage("migrate")
+
+        manage = manage_factory()  # value from MANGAGE_PY_PATH envvar
+        manage("mirgrate")
     :param manage_py_path: Path to the project's manage.py file.
     :return: callable
     """
@@ -16,7 +22,14 @@ def manage_factory(manage_py_path: str):
         :param args: options and/or arguments to the command
         :return:
         """
-        argv = [manage_py_path, command] + list(args)
+        try:
+            _manage_py_path = manage_py_path or os.environ["MANAGE_PY_PATH"]
+        except KeyError as exc:
+            raise ValueError(
+                "Either call manage_factory with an explicit path or set the "
+                "MANAGE_PY_PATH environment variable."
+            ) from exc
+        argv = [_manage_py_path, command] + list(args)
         return execute_from_command_line(argv)
 
     return _manage
